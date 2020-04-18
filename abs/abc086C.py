22e94c162acb41e13f8323a7c9de4e5e7f56bcf1
tabineko <jiyuujinn.mu11@gmail.com>
2020-04-18 09:36:07 +0900

    Done!

---
+++ abs/abc086C.py
@@ -0,0 +1,26 @@
+N = int(input())
+x = [0]
+y = [0]
+t = [0]
+for _ in range(N):
+    a, b, c = map(int, input().split())
+    t.append(a)
+    x.append(b)
+    y.append(c)
+
+poss = "Yes"
+
+for i in range(N):
+    d = abs(x[i+1]-x[i]) + abs(y[i+1]-y[i])
+    td = t[i+1]-t[i]
+    if td < d:
+        poss = "No"
+        break
+    if d % 2 == 0 and td % 2 == 1:
+        poss = "No"
+        break
+    if d % 2 == 1 and td % 2 == 0:
+        poss = "No"
+        break
+
+print(poss)
